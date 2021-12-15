4d55adb0f1f7fcc4ae51d7cb4f51ca68f5b34b35
Christopher Petrinovich <petrinovichch@gmail.com>
2021-12-14 23:08:15 -0700

    second commit

--- javascript/index.js
+++ javascript/index.js
@@ -24,8 +24,8 @@ let months =[
 
 let d = new Date();
 let dayName = daynames[d.getDay()];
-let monthName = month[d.getMonth()];
-let year = d.getFullYear;
-let fulldate = dayName + ", " + monthName + " " + d.getDate + ", " + year;
+let monthName = months[d.getMonth()];
+let year = d.getFullYear();
+let fulldate = dayName + ", " + monthName + " " + d.getDate() + ", " + year;
 
 document.getElementById("currentdate").textContent = fulldate;
